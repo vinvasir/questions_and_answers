a27dfb5e0e678bdf51052e42bf08f97feec21082
Prateek Vasireddy <prateek@dave.com>
2019-08-16 16:56:23 -0700

    Question model and migration

---
+++ database/migrations/2019_08_16_235143_create_questions_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateQuestionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('questions', function (Blueprint $table) {
+            $table->bigIncrements('id');
+
+            /**
+             * For now this column will not have an index
+             * because the requirements don't call for
+             * searching, sorting, or filtering
+             * by question body or content
+             */
+            $table->text('body');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('questions');
+    }
+}
