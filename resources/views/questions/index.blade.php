3a0ed5de09c86b0b321600f715e23971311dd6bf
Prateek Vasireddy <prateek@dave.com>
2019-08-16 17:37:14 -0700

    add basic form

--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -1,6 +1,16 @@
 @extends('layouts.app')
 
 @section('content')
+    <form action="/questions" method="POST" class="border-bottom p-3">
+        @csrf
+        <div class="form-group">
+            <textarea name="body" id="body"  class="form-control"></textarea>
+        </div>
+        <div class="text-center">
+            <button class="btn btn-primary" type="submit">Just ask</button>
+        </div>
+    </form>
+
     <h2 class="bg-primary p-5 text-center text-white">Questions</h2>
 
     @foreach($questions as $question)
